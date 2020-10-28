905031433242af8249f1e3df1ee95e7e03c5e6a2
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-10-27 20:38:16 -0500

    added attr_reader for all arguments/ added attr_writer for all arguments

--- cars.rb
+++ cars.rb
@@ -1,4 +1,15 @@
 class Cars
+  attr_reader :make
+  attr_reader :model
+  attr_reader :color
+  attr_reader :year
+  attr_reader :price
+  attr_writer :make
+  attr_writer :model
+  attr_writer :color
+  attr_writer :year
+  attr_writer :price
+  
   def initialize(make, model, color, year, price)
     @make = make
     @model = model
@@ -7,9 +18,7 @@ class Cars
     @price = price
   end
 
-  # def make
-  #   @make
-  # end
+
 
   def print_info
     p "This vehicle is a #{@color} #{@year} #{@make} #{@model} that's valued at $#{@price} "
@@ -18,4 +27,6 @@ end
 
 car1 = Cars.new("Ford", "Mustang", "Cherry Red", 1972, 45000)
 
+
+car1.color = "Lime Green"
 car1.print_info
